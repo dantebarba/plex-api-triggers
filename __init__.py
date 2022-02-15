5c089fe6e9e50dc517e889826efd3b53031525cb
Dante Barba <dantebarba@gmail.com>
2022-02-14 21:17:24 -0300

    Added loglevel

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
 import logging
+import os
 import plex_api
 
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=eval(os.getenv("LOG_LEVEL", "logging.WARNING")))
 plex_events = plex_api.PlexEventHandler()
 plex_events.start_polling()
